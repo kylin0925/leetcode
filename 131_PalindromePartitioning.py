93ad22ea4247f0889b6153c76d45e2616888eb24
kylin <kylin25@gmail.com>
2021-07-24 09:22:14 +0800

    leetcode 131 v2

--- 131_PalindromePartitioning.py
+++ 131_PalindromePartitioning.py
@@ -1,6 +1,6 @@
 from typing import List
 
-class Solution:
+class Solution2:
     def partition(self, s: str) -> List[List[str]]:
         self.ans = []
         self.tmp = []
@@ -25,7 +25,47 @@ class Solution:
                     self.tmp.pop()
         dfs(0)
         return self.ans
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        self.ans = [ [] for i in range(len(s))]
+        self.tmp = []
 
-print(Solution().partition(s = "aab"))
-print(Solution().partition(s = "a"))
-print(Solution().partition(s = "aaaaaaaaaaaaaaaa"))
+        def check(s):
+            #print(s)
+            mid = len(s)>>1
+            l = len(s)
+            for i in range(0,mid):
+                if s[i]!=s[l-i-1]:
+                    return False
+            return True
+
+        def dfs(i):
+            if i == len(s):
+                #self.ans.append(self.tmp[:])
+                #print(self.tmp,self.ans)
+                return [[]]
+            tmp = []
+            if len(self.ans[i]) > 0 :
+                #print("get cache")
+                return self.ans[i]
+            for j in range(1,len(s)+1):
+                p = s[i:j+i]
+                if j+i <= len(s) and check(p):
+                    subans = dfs(j+i)
+                    for r in subans:
+                            tmp.append([p] + r)
+            self.ans[i] = tmp
+            return tmp
+        dfs(0)
+        #print(self.ans)
+        return self.ans[0]
+import time
+start = time.time()
+#print(Solution().partition(s = "aab"))
+#print(Solution().partition(s = "a"))
+
+print(Solution().partition(s = "aaaaaaaaaaaaaaaa") == Solution2().partition(s = "aaaaaaaaaaaaaaaa"))
+print(Solution().partition(s = "abceecba") == Solution2().partition(s = "abceecba"))
+#print(Solution().partition(s = "aaaa"))
+print(time.time() - start)
+
