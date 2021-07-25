236233f78467b21b8840695624d4f8bf939c9c69
kylin <kylin25@gmail.com>
2021-07-25 20:42:56 +0800

    update leetcode 279

--- 279_PerfectSquares.py
+++ 279_PerfectSquares.py
@@ -1,28 +1,43 @@
+import time
+# class Solution:
+#     tbl = []
+#     def numSquares(self, n: int) -> int:
+#         start = time.time()
+#         maxsq = int(n**0.5)
+#         tbl = [[0 for i in range(n+1)] for j in range(maxsq+1)]
+#         tbl[1] = [i for i in range(n+1)]
+#         print("arr init",time.time() - start)
+#         for p in range(2,maxsq+1):
+#             p2 = p**2
+#             for i in range(1, n+1):
+#                 if i < p2:
+#                     tbl[p][i] = tbl[p-1][i]
+#                 else:
+#                     r = i - p2
+#                     c = tbl[p][r] + 1
+#                     tbl[p][i] = min(c, tbl[p-1][i])
+#         #print(tbl)
+#         return tbl[maxsq][n]
 class Solution:
+    tbl = [0]
     def numSquares(self, n: int) -> int:
+        for i in range(1, n+1):
+            p2 = 1
+            tmp =i
+            while p2*p2 <= i:
+                r = i - p2*p2
+                tmp = min(self.tbl[r] + 1,tmp)
+                p2+=1
 
-        maxsq = int(n**0.5)
-        tbl = [[0 for i in range(n+1)] for j in range(maxsq+1)]
-        tbl[1] = [i for i in range(n+1)]
-
-        for p in range(2,maxsq+1):
-            p2 = p**2
-            for i in range(1, n+1):
-                if i < p2:
-                    tbl[p][i] = tbl[p-1][i]
-                else:
-                    r = i - p2
-                    c = tbl[p][r] + 1
-                    tbl[p][i] = min(c, tbl[p-1][i])
-        #print(tbl)
-        return tbl[maxsq][n]
-
+            self.tbl.append(tmp)
+        #print(self.tbl)
+        return self.tbl[n]
 
 print(Solution().numSquares(12))
 print(Solution().numSquares(16))
 # print(Solution().numSquares(21))
 # print(Solution().numSquares(999))
-import time
+
 start = time.time()
 print(Solution().numSquares(10000))
 print(time.time() - start)
